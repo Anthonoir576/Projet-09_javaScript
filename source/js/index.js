6fb91bd23e45f6aca05f662a0855d8cbb7d54c44
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-07-17 17:58:33 +0200

    js

---
+++ source/js/index.js
@@ -0,0 +1,26 @@
+let imgCurseur = document.querySelector('.cursor');
+
+// Permet de faire suivre un curseur au mouvement de la souris
+window.addEventListener('mousemove', cursor);
+
+function cursor(event) {
+
+    imgCurseur.style.top = event.pageY + 'px';
+    imgCurseur.style.left = event.pageX + 'px';
+    
+}
+
+// animation au curseur
+let navLinks = document.querySelectorAll('.nav-links li');
+
+// boucle pour parcourir les li 
+navLinks.forEach(lien => {
+
+    lien.addEventListener('mouseover', () => {
+
+        imgCurseur.classList.add('link-grow');
+        lien.classList.add("hover-link");
+
+    })
+
+})
